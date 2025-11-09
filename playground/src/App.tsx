55072ebf8ad9a4936d7cebf79571033df572e350
Jaganath M S <jagan.msjc@gmail.com>
2025-11-09 15:43:08 +0530

    fix(Playground): update Calendar component with new props
    Add props to demonstrate Calendar component functionality including dayType, data, dimensions, selectedDate, and year range controls

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,8 +1,17 @@
-import Calendar, { EDayType, DAY_TYPE } from "../../src";
+import Calendar, { EDayType } from "../../src";
 
 function App() {
   return (
-      <Calendar />
+    <Calendar
+      dayType={EDayType.fullName}
+      data={["22", "", "92", "", "", "829", "", "", "", "2810"]}
+      width={700}
+      height={500}
+      selectedDate={new Date('2025-11-16')}
+      isSelectDate
+      pastYearLength={2}
+      futureYearLength={3}
+    />
   );
 }
 
